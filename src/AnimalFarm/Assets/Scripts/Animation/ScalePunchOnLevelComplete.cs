8e5c41f190c223c025b8d3386016b96ac1faf4f3
SilasReinagel <silas.reinagel@gmail.com>
2023-06-19 16:05:46 -0500

    Coding: Barn Scale Punch on Finished. Closes #15

---
+++ src/AnimalFarm/Assets/Scripts/Animation/ScalePunchOnLevelComplete.cs
@@ -0,0 +1,15 @@
+﻿
+using DG.Tweening;
+using UnityEngine;
+
+public class ScalePunchOnLevelComplete : OnMessage<LevelCompleted>
+{
+    [SerializeField] private float amount;
+    [SerializeField] private float duration;
+    [SerializeField] private GameObject punchTarget;
+    
+    protected override void Execute(LevelCompleted msg)
+    {
+        punchTarget.transform.DOPunchScale(new Vector3(amount, amount, amount), duration, 1);
+    }
+}
--- src/AnimalFarm/Assets/Scripts/UI/CenterOnLevel.cs
+++ src/AnimalFarm/Assets/Scripts/UI/CenterOnLevel.cs
@@ -6,7 +6,10 @@ public class CenterOnLevel : OnMessage<LevelReset>
     [SerializeField] private CurrentLevel level;
     [SerializeField] private Boolean3 dimensions;
     [SerializeField] private Vector3 offset;
-
+    [SerializeField] private float minHeight;
+    [SerializeField] private float maxHeight;
+    [SerializeField] private float scaleForWidth = 0.2f;
+    
     private void Start() => Center();
 
     protected override void Execute(LevelReset msg) => Center();
@@ -16,13 +19,22 @@ public class CenterOnLevel : OnMessage<LevelReset>
         if (level.ActiveLevel == null)
             return;
         
-        var bounds = level.ActiveLevel.Prefab.GetComponentsInChildren<Renderer>().Select(x => x.bounds);
+        var bounds = level.ActiveLevel.Prefab.GetComponentsInChildren<Renderer>().Where(x => !x.gameObject.tag.Contains("LevelOverflow")).Select(x => x.bounds);
         var boundsCombined = bounds.First();
         bounds.ForEach(x => boundsCombined.Encapsulate(x));
         var x = (dimensions.X ? boundsCombined.center.x : transform.position.x) + offset.x;
         var y = (dimensions.Y ? boundsCombined.center.y : transform.position.y) + offset.y;
         var z = (dimensions.Z ? boundsCombined.center.z : transform.position.z) + offset.z;
+
+        var width = boundsCombined.size.x;
+        var upVectorIsY = !dimensions.Y;
+        var height = upVectorIsY ? transform.position.y : transform.position.z;
+        var heightScale = height * scaleForWidth;
+        var scaledHeight = Mathf.Clamp(heightScale * width, minHeight, maxHeight);
         
-        transform.position = new Vector3(x, y, z);
+        var finalY = upVectorIsY ? scaledHeight : y;
+        var finalZ = !upVectorIsY ? scaledHeight : z;
+        
+        transform.position = new Vector3(x, finalY, finalZ);
     }
 }
