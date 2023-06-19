39389d0d7cbac36fb70600c9e5c5167fde5a91fb
SilasReinagel <silas.reinagel@gmail.com>
2023-06-19 11:08:04 -0500

    Coding: Center on Level

---
+++ src/AnimalFarm/Assets/Code/Boolean3.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public struct Boolean3
+{
+    [SerializeField] private bool x;
+    [SerializeField] private bool y;
+    [SerializeField] private bool z;
+    
+    public bool X => x;
+    public bool Y => y;
+    public bool Z => z;
+}
--- src/AnimalFarm/Assets/Scripts/UI/CenterOnLevel.cs
+++ src/AnimalFarm/Assets/Scripts/UI/CenterOnLevel.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class CenterOnLevel : OnMessage<LevelReset>
 {
     [SerializeField] private CurrentLevel level;
+    [SerializeField] private Boolean3 dimensions;
+    [SerializeField] private Vector3 offset;
 
     private void Start() => Center();
 
@@ -17,6 +19,10 @@ public class CenterOnLevel : OnMessage<LevelReset>
         var bounds = level.ActiveLevel.Prefab.GetComponentsInChildren<Renderer>().Select(x => x.bounds);
         var boundsCombined = bounds.First();
         bounds.ForEach(x => boundsCombined.Encapsulate(x));
-        transform.position = new Vector3(boundsCombined.center.x, boundsCombined.center.y, transform.position.z);
+        var x = (dimensions.X ? boundsCombined.center.x : transform.position.x) + offset.x;
+        var y = (dimensions.Y ? boundsCombined.center.y : transform.position.y) + offset.y;
+        var z = (dimensions.Z ? boundsCombined.center.z : transform.position.z) + offset.z;
+        
+        transform.position = new Vector3(x, y, z);
     }
 }
