671fa52c0c91f1b0ab72593a7756fa31b787fe10
SilasReinagel <silas.reinagel@gmail.com>
2023-07-08 15:38:15 -0500

    Art: Added 8 more Hero Animals. Closes #60. Closes #61

---
+++ src/AnimalFarm/Assets/Code/RandomlyEnableOneOnAwake.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RandomlyEnableOneOnAwake : MonoBehaviour
+{
+    [SerializeField] private GameObject[] options;
+
+    private void Awake()
+    {
+        options.ForEach(o => o.SetActive(false));
+        options.Random().SetActive(true);
+    }
+}
