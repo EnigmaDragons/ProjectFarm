61613075edc8b6f58d1db02f56c0c6f00f3dc143
SilasReinagel <silas.reinagel@gmail.com>
2023-07-10 12:03:25 -0500

    Temp: Disabled Post-Pro on iOS

---
+++ src/AnimalFarm/Assets/Code/Platforms/DisableOnIos.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class DisableOnIos : MonoBehaviour
+{
+    [SerializeField] private MonoBehaviour[] targets;
+    
+    #if UNITY_IOS
+    private void Awake()
+    {
+        foreach (var target in targets)
+            target.enabled = false;
+    }
+    #endif
+}
