07596e61f4fcbdef0a221b39327c6ad4a9e79cb7
SilasReinagel <silas.reinagel@gmail.com>
2023-07-18 15:25:43 -0500

    Art: Juice on Animal Selection Changed. Closes #172

---
+++ src/AnimalFarm/Assets/Scripts/VFX/ParticlesOnAnimalSelected.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class ParticlesOnAnimalSelected : OnMessage<HeroAnimalSelected>
+{
+    [SerializeField] private ParticleSystem particle;
+    [SerializeField] private GameObject target;
+    
+    protected override void Execute(HeroAnimalSelected msg)
+    {
+        if (msg.Selected == HeroAnimal.NotSelected)
+        {
+            target.SetActive(false);
+            return;
+        }
+        
+        target.SetActive(true);
+        particle.Stop();
+        particle.Play();
+    }
+}
