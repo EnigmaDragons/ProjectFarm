4b997665ee74817333f1399292647386ddc0fbb8
SilasReinagel <silas.reinagel@gmail.com>
2023-07-20 22:48:03 -0500

    Bug Fix: Animal Selection not triggering at the right time fixe. Closes #175

--- src/AnimalFarm/Assets/Scripts/UI/AnimalSelect/HideDuringAnimalSelection.cs
+++ src/AnimalFarm/Assets/Scripts/UI/AnimalSelect/HideDuringAnimalSelection.cs
@@ -2,18 +2,31 @@
 
 public class HideDuringAnimalSelection : OnMessage<AnimalSelectionBegun, AnimalSelectionFinished>
 {
+    [SerializeField] private bool hideOnStart = true;
     [SerializeField] private GameObject[] targets;
     [SerializeField] private GameObject[] showTargets;
+
+    private void Awake()
+    {
+        if (hideOnStart)
+            SetForAnimalSelection();
+        else
+            SetForNonAnimalSelection();
+    }
     
-    protected override void Execute(AnimalSelectionBegun msg)
+    protected override void Execute(AnimalSelectionBegun msg) => SetForAnimalSelection();
+    protected override void Execute(AnimalSelectionFinished msg) => SetForNonAnimalSelection();
+
+    private void SetForAnimalSelection()
     {
         targets.ForEach(x => x.SetActive(false));
         showTargets.ForEach(x => x.SetActive(true));
     }
 
-    protected override void Execute(AnimalSelectionFinished msg)
+
+    private void SetForNonAnimalSelection()
     {
-        targets.ForEach(x => x.SetActive(true)); 
+        targets.ForEach(x => x.SetActive(true));
         showTargets.ForEach(x => x.SetActive(false));
     }
 }
